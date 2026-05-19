d6aec045049161c46a335a3356aa68efb2050bd3
Pablo <pfeijo34@gmail.com>
2026-05-19 12:58:36 -0500

    Git  interfaces v2.0

--- src/main/java/ec/edu/ups/ejemplointerfaces/EjemploInterfaces.java
+++ src/main/java/ec/edu/ups/ejemplointerfaces/EjemploInterfaces.java
@@ -4,15 +4,11 @@
 
 package ec.edu.ups.ejemplointerfaces;
 import ec.edu.ups.ejemplointerfaces.EsquemaRaton;
-import java.awt.Frame;
 import ec.edu.ups.ejemplointerfaces.MiVentana;
-import java.awt.Point;
-import java.awt.Button;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-import java.awt.Label;
-import java.awt.Panel;
+import ec.edu.ups.ejemplointerfaces.InterrupcionesDeRaton;
+import ec.edu.ups.ejemplointerfaces.InterrupcionesDeTeclado;
+import ec.edu.ups.ejemplointerfaces.InterrupcionesDeVentana;
+import ec.edu.ups.ejemplointerfaces.InterrupcionesDeAction;
 import java.awt.*;
 /**
  *
@@ -58,6 +54,72 @@ public class EjemploInterfaces {
           miMarco.setVisible(true);
           botonEntrada.addMouseListener(new EsquemaRaton());
           botonSalida.addMouseListener(new EsquemaRaton());
+          
+//          final int NUM_OPCIONES = 12;
+//          Label mensaje = new Label("Mensaje asocialdo al boton pulsado");
+//          Button[] botones = new Button[NUM_OPCIONES];
+//          for(int i = 0;i<NUM_OPCIONES; i++){
+//              botones[i] = new Button("Opcion" + i);
+//              if(i<10){
+//                  botones[i].setName("0" + i);
+//              }else{
+//                  botones[i].setName(String.valueOf(i));
+//              }
+//          }
+//          Frame miVentanaUno = new Frame("Pruebas eventos del raton");
+//          Panel panelPrincipal = new Panel(new GridLayout(NUM_OPCIONES+1,1));
+//          panelPrincipal.add(mensaje);
+//          for(int i=0;i<NUM_OPCIONES;i++){
+//              panelPrincipal.add(botones[i]);
+//          }
+//          miVentanaUno.add(panelPrincipal);
+//          miVentanaUno.setSize(400, 600);
+//          miVentanaUno.setVisible(true);
+//          for(int i = 0;i<NUM_OPCIONES;i++){
+//              botones[i].addMouseListener(new InterrupcionesDeRaton(mensaje));
+//          }
+//          Frame miVentanaDos = new Frame("Prueba eventos de raton");
+//          Panel miPanelDos = new Panel();
+//          Button boton1 = new Button("Silecion");
+//          Button boton2 = new Button("Maquina de escribir");
+//          miPanelDos.add(boton1);
+//          miPanelDos.add(boton2);
+//          boton2.addKeyListener(new InterrupcionesDeTeclado());
+//          miVentanaDos.add(miPanelDos);
+//          miVentanaDos.setSize(400,400);
+//          miVentanaDos.setVisible(true);
+          
+//          Frame miVentanaTres = new Frame("Prueba eventos de ventana");
+//          miVentanaTres.setSize(200,100);
+//          miVentanaTres.setVisible(true);
+//          
+//          Frame otraVentana = new Frame("Prueba eventos de ventana");
+//          otraVentana.setSize(200,100);
+//          otraVentana.setVisible(true);
+//          
+//          miVentanaTres.addWindowListener(new InterrupcionesDeVentana());
+//          otraVentana.addWindowListener(new InterrupcionesDeVentana());
+            Frame miVentanaCuatro = new Frame("Prueba eventos de accion");
+            Panel miPanelTres = new Panel(new GridLayout(3,1));
+            Button botonA = new Button("Etiqueta del boton");
+            TextField campoDeTexto = new TextField("Texto de Campo 8");
+            List lista = new List(3);
+            lista.add("Opcion 1 de la lista");
+            lista.add("Opcion 2 de la lista");
+            lista.add("Opcion 3 de la lista");
+            lista.add("Opcion 4 de la lista");
+            miPanelTres.add(botonA);
+            miPanelTres.add(campoDeTexto);
+            miPanelTres.add(lista);
+            
+            botonA.addActionListener(new InterrupcionesDeAction());
+            campoDeTexto.addActionListener(new InterrupcionesDeAction());
+            lista.addActionListener(new InterrupcionesDeAction());
+            
+            miVentanaCuatro.add(miPanelTres);
+            miVentanaCuatro.setSize(400,200);
+            miVentanaCuatro.setVisible(true);
+            
           
         
       
